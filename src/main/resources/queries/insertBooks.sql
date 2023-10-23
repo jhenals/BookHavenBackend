de4e448d42648479b7b51db1fb39cb92ab175f19
Jhenals <suboljhenalyn3@gmail.com>
2023-10-23 23:37:34 +0200

    add insertBooks query

---
+++ src/main/resources/queries/insertBooks.sql
@@ -0,0 +1,11 @@
+INSERT INTO book (title, author, image_url, category_id) VALUES
+('Jurassic Park',' Michael Crichton','https://m.media-amazon.com/images/I/51Sh8gB0bfL._SY445_SX342_.jpg', 10),
+('Winnie-The-Pooh', 'A.A. Milne', 'https://m.media-amazon.com/images/I/414RfRBVz1L._SY445_SX342_.jpg', 7),
+('They Wish They Were Us', 'Jessica Goodman', 'https://m.media-amazon.com/images/I/51H0Ej6XoiL._SY445_SX342_.jpg', 6 ),
+('Killers of the Flower Moon: The Osage Murders and the Birth of the FBI', 'David Grann', 'https://m.media-amazon.com/images/I/514QUJzXGZL._SY445_SX342_.jpg', 5),
+('Milk and Honey', 'Rupi Kaur', 'https://m.media-amazon.com/images/I/71Eq55V2AvL._SY425_.jpg', 9),
+('Never Lie: An addictive psychological thriller', 'Freida McFadden', 'https://m.media-amazon.com/images/I/61UdOEvyVjL._SY425_.jpg', 4),
+('Beach Read', 'Emily Henry', 'https://m.media-amazon.com/images/I/71zSiH2uWcL._SY425_.jpg', 2 ),
+('Persepolis:The Story of a Childhood ', 'Marjane Satrapi', 'https://m.media-amazon.com/images/I/81iniAfNnxL._SY425_.jpg', 8),
+('Where the Crawdads Sing', 'Delia Owens', 'https://m.media-amazon.com/images/I/516XEz3XX4L._SY445_SX342_.jpg', 11 ),
+('Little Women','Louisa May Alcott','https://m.media-amazon.com/images/I/51JAgOJfaKL._SY445_SX342_.jpg', 1);
