d4f98b9614be7cc1b0ace5a700c213e01481b576
Jhenals <suboljhenalyn3@gmail.com>
2023-10-28 11:14:40 +0200

    update Category entity

--- src/main/java/com/progetto/BookHavenBackend/entities/Category.java
+++ src/main/java/com/progetto/BookHavenBackend/entities/Category.java
@@ -17,8 +17,8 @@ public class Category {
     @Column(name = "name")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "book_id")
-    private Book book;
+    //@ManyToOne
+    //@JoinColumn(name = "book_id")
+    //private Book book;
 
 }
